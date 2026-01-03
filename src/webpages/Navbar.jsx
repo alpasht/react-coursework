a30f353c0879e551e6d5f4b8113fb6065f265489
Imran Maiwandi <imranmaiwandi@gmail.com>
2026-01-03 19:43:37 +0000

    added responsive hamburger menu and removed obsolete 'links' function for Link @react-router-dom

--- src/webpages/Navbar.jsx
+++ src/webpages/Navbar.jsx
@@ -1,29 +1,35 @@
 // wip add href links
+import { useState } from 'react';
 import './Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import Logo from '../assets/minimalist-logo.jpg'
 import ThemeWidget from '../../components/ThemeWidget';
 
 function Navbar() {
 
-    const links = [
-        { href: "/", label: "Home" },
-        { href: "/property", label: "Browse Properties" },
-        { href: "/contact", label: "Contact Us" },
-        { href: "/about", label: "About Us" },
-    ];
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <>
             <nav className="navbar-container">
                 <div className="navbar">
+                    <img src={Logo} alt="minimalist logo" className="logo" />
 
-                    <img src={Logo} alt="minimalist logo"></img>
+                    <div className="hamburger" onClick={toggleMenu}>
+                        <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} />
+                    </div>
 
-                    <ul className="nav-links">
-                        <li> <Link to="/">Home </Link> </li>
-                        <li> <Link to="/"> Properties </Link> </li>
-                        <li> <Link to="/">Contact </Link> </li>
-                        <li> <Link to="/">Lorem </Link> </li>
+                    <ul className={`nav-links ${isMenuOpen ? "active" : ""}`}>
+                        <li> <Link to="/" onClick={() => setIsMenuOpen(false)}>Home</Link> </li>
+                        <li> <Link to="/property" onClick={() => setIsMenuOpen(false)}>Properties</Link> </li>
+                        <li> <Link to="/contact" onClick={() => setIsMenuOpen(false)}>Contact</Link> </li>
+                        <li> <Link to="/about" onClick={() => setIsMenuOpen(false)}>About Us</Link> </li>
                         <li> <ThemeWidget /> </li>
                     </ul>
                 </div>
